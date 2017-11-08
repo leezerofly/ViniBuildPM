72651827a1cb9f73babdf9bef0ba40780ba0ebdc
yujinghu <932868633@qq.com>
2017-11-08 19:40:27 +0800

    task

--- 胡雨婧-信息页/messagePage/src/app/app.module.ts
+++ 胡雨婧-信息页/messagePage/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { MessagePage} from '../pages/message/message';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -14,10 +13,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MessagePage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +24,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MessagePage
   ],
   providers: [
     StatusBar,
