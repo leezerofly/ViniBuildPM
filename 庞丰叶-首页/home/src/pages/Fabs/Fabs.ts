43d6d02092fc11c5b1981582aafca9c918e053b9
PangFengYe <2489978237@qq.com>
2017-11-08 20:48:32 +0800

    pangfengye

---
+++ 庞丰叶-首页/home/src/pages/Fabs/Fabs.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+
+@Component({
+selector : 'Fabs',
+  templateUrl: 'Fabs.html'
+})
+export class FabsPage { }
---
+++ 庞丰叶-首页/home/src/pages/list/list.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, Platform  } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  icons: string[];
+  pet: string = "puppies";
+  isAndroid: boolean = false;
+
+  
+  items: Array<{title: string, note: string, icon: string,}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,platform: Platform) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+    this.isAndroid = platform.is('android');
+    // Let's populate this page with some filler content for funzies
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+
+      });
+
+    }
+  }
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(ListPage, {
+      item: item
+    });
+  }
+  
+}
