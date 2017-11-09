f4bd09506917c5850870b2dd9f86b6b0d5f37606
unknown <2310226060@qq.com>
2017-11-09 16:52:47 +0800

    添加点击事件

--- 潘小琴-通知页/TopicPagePan/src/pages/about/about.ts
+++ 潘小琴-通知页/TopicPagePan/src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {  ModalController, Platform, NavParams, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +7,95 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public modalCtrl: ModalController) {  
 
   }
 
-}
+  openModal(characterNum) {
+    
+        let modal = this.modalCtrl.create(ContentPage, characterNum);
+        modal.present();
+      }
+    }
+    
+    @Component({
+      template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          话题详情
+        </ion-title>
+
+        <ion-buttons start>
+          <button ion-button (click)="dismiss()">
+            <span ion-text color="primary" showWhen="ios">Cancel</span>
+            <ion-icon name="md-close" showWhen="android, windows"></ion-icon>
+          </button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+
+      <ion-list>
+          <ion-item>
+            <ion-avatar item-start>
+              <img src="{{character.image}}">
+            </ion-avatar>
+
+            <h2>{{character.name}}</h2>
+            <p>{{character.quote}}</p>
+          </ion-item>
+
+
+          <ion-item>
+           <p>{{character.items}}</p>
+          </ion-item>
+      </ion-list>
+
+    </ion-content>
+    `
+    })
+    export class ContentPage {
+      character;
+    
+      constructor(
+        public platform: Platform,
+        public params: NavParams,
+        public viewCtrl: ViewController
+      ) {
+        var characters = [
+          {
+            name: 'Gollum',
+            quote: '2017/11/8',
+            image: '',
+            items: '与我相关'
+          },
+          {
+            name: 'Frodo',
+            quote: 'Go back, Sam! I\'m going to Mordor alone!',
+            image: '',
+            items: [
+              { title: 'Race', note: 'Hobbit' },
+              { title: 'Culture', note: 'Shire Folk' },
+              { title: 'Weapon', note: 'Sting' }
+            ]
+          },
+          {
+            name: 'Samwise Gamgee',
+            quote: 'What we need is a few good taters.',
+            image: '',
+            items: [
+              { title: 'Race', note: 'Hobbit' },
+              { title: 'Culture', note: 'Shire Folk' },
+              { title: 'Nickname', note: 'Sam' }
+            ]
+          }
+        ];
+        this.character = characters[this.params.get('charNum')];
+      }
+    
+      dismiss() {
+        this.viewCtrl.dismiss();
+      }
+    }
