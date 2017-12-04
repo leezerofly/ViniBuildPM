14e24466f5ecf4afd4eb25336ac074959398a691
leezerofly <leezerofly@gmail.com>
2017-12-04 15:12:26 +0800

    删除ViniBuildPM,准备重构

--- ViniBuildPM/src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { PersonMessageComponent } from './person-message/person-message';
-@NgModule({
-	declarations: [PersonMessageComponent],
-	imports: [],
-	exports: [PersonMessageComponent]
-})
-export class ComponentsModule {}
