04e0c089eb90d26b063f572609f005ab8385839b
yangmeng611 <1558647190@qq.com>
2018-05-26 18:19:21 +0800

    完善

--- ViniBuildPM/src/pages/clock/clock.ts
+++ ViniBuildPM/src/pages/clock/clock.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ClockPage page.
@@ -14,8 +15,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'clock.html',
 })
 export class ClockPage {
+  time1: any;
+  time2: any; 
+  type : string = "上班打卡";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public alerCtrl: AlertController) {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month,day ;
+    month = date.getMonth() + 1;
+    if(month >= 1 && month <= 9){
+      month = "0"+month;
+      console.log(typeof (month))
+    }
+    day = date.getDate();
+    if(day >= 0 && day <= 9){
+      day = "0"+day;
+    }
+    var hour,minute,second;
+    hour = date.getHours();
+    if(hour >= 0 && hour <= 9){
+      hour = "0" + hour;
+      console.log(typeof (hour))
+    }
+    minute = date.getMinutes();
+    if(minute >= 0 && minute <= 9){
+      minute = "0" + minute;
+      console.log(typeof (minute));
+    }
+    second = date.getSeconds();
+    if(second >= 0 && second <= 9){
+      second = "0" + second;
+      console.log(typeof (second));
+    }
+    this.time1 = year+'-'+month+"-"+day;
+    this.time2 = hour+':'+minute+':'+second;
+  }
+
+  doAlert() {
+    let alert = this.alerCtrl.create({
+      title: '打卡成功',
+    });
+    alert.present()
+    if(type=="上班打卡")
+      type:string = "下班打卡"
   }
 
   ionViewDidLoad() {
--- ViniBuildPM/src/pages/user/changePwd/changePwd.ts
+++ ViniBuildPM/src/pages/user/changePwd/changePwd.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'changePwd',
@@ -11,8 +13,9 @@ export class ChangePwd {
   passwordDis2:boolean=false
   passwordDis3:boolean=false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public alerCtrl: AlertController
+  ) { }
 
   toSecondStep() {
     this.passwordDis1=false;
@@ -24,14 +27,39 @@ export class ChangePwd {
     this.passwordDis3=true;
   }
   
-  returnFirstStep(){
+  returnFirstStep() {
     this.passwordDis1=true;
     this.passwordDis2=false;
   }
   
-  toForthStep(){
+  toForthStep() {
     this.passwordDis1=true;
     this.passwordDis3=false;
+  }
+
+
+  doConfirm() {
+    let confirm = this.alerCtrl.create({
+      // title: 'Use this lightsaber?',
+      message: '确认修改密码？',
+      buttons: [
+        {
+          text: '确认',
+          handler: () => {
+            this.passwordDis2=false;
+            this.passwordDis3=true;
+          }
+        },
+        {
+          text: '取消',
+          handler: () => {
+            this.passwordDis1=true;
+            this.passwordDis2=false;
+          }
+        }
+      ]
+    });
+    confirm.present()
   }
 
 }
